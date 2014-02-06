a755739c2d21c2eceacdd2a9e45af4361f32765b
barachenadia <barache.nadia@yahoo.fr>
2014-02-06 16:36:46 +0100

    modifi notifier

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,9 +1,9 @@
 class Notifier < ActionMailer::Base
-  default from: "@user.email"
+  default :from => @user.email
  
  def send_signup_email(user)
     @user = user
-    mail( :to => 'choralchoeurdejoie@gmail.com',
+    mail( :to => "choralchoeurdejoie@gmail.com",
     :subject => 'Thanks for signing up for our amazing app' )
   end
 
