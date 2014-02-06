7c493a68d398ff9eeaf5d0b93984dc910528dd6c
barachenadia <barache.nadia@yahoo.fr>
2014-02-06 15:59:45 +0100

    first commit

---
+++ app/mailers/notifier.rb
@@ -0,0 +1,10 @@
+class Notifier < ActionMailer::Base
+  default from: "choralchoeurdejoie@gmail.com"
+ 
+ def send_signup_email(user)
+    @user = user
+    mail( :to => @user.email,
+    :subject => 'Thanks for signing up for our amazing app' )
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sendgridmail::Application.config.secret_key_base = '06e978d88f82739864535d992f5ebc84af45e2686d1c5ba8bbadb8743ccc53582d5b4520da62fd5a6af941e373d4a8ebb9c4910eb4eb3a4f220f1bb4b2552c10'
